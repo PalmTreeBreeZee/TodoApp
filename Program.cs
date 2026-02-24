8e42ec40f30b3137904643880e7fbdd3c7a4ff6a
Azeem Brown <azeembrown717@gmail.com>
2026-02-24 10:55:13 -0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using TodoApp.Context;
+using Microsoft.EntityFrameworkCore;
+using TodoApp.Repositories;
+using TodoApp.Services;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContextFactory<TodoContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection")));
+});
+builder.Services.AddControllers();
+builder.Services.AddScoped<TodoRepository>();
+builder.Services.AddScoped<TodoService>();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
